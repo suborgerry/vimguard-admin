2f0e53456754af3e79d2700961817dd9d2483bd7
borgerry <borodingerry@gmail.com>
2021-08-06 15:45:13 +0300

    Main products list init

--- scripts/admin-scripts.js
+++ scripts/admin-scripts.js
@@ -18,3 +18,26 @@ if(passwordShowBtn) {
         fieldPassword.type = "password";
     });
 }
+
+const productContainer = document.querySelector('.main_page_product_list_main');
+if(productContainer) {
+    productContainer.addEventListener('change', evt => {
+        if(evt.target.matches('input')) { checkProduct(evt.target) }
+    });
+
+    function checkProduct(input) {
+        const checkboxes  = productContainer.querySelectorAll('.product_list_item_checkbox');
+        const downloadBtn = document.querySelector('.main_page_product_list_footer_btn');
+        
+        for(let i = 0; i < checkboxes.length; i++) {
+            if(checkboxes[i].checked) {
+                downloadBtn.classList.remove('disabled');
+                downloadBtn.disabled = false;
+                break
+            } else {
+                !downloadBtn.classList.contains('disabled') && downloadBtn.classList.add('disabled');
+                downloadBtn.disabled = true;
+            }
+        };
+    }
+}
