1d294298dcc3e9dd86d3c579ed92488257ba1a4a
borgerry <borodingerry@gmail.com>
2021-08-11 17:11:53 +0300

    Account functions

--- scripts/admin-scripts.js
+++ scripts/admin-scripts.js
@@ -1,23 +1,37 @@
-const passwordShowBtn = document.querySelector('.password_show_btn');
+// Выглядит сложно но для теперь, чтобы показать/скрыть пароль
+// достаточно соблюсти простую конструкцию в html:
+// <div>
+//     <input type="password" input_password" id="password" placeholder="Password">
+//     <button type="button" class="password_show_btn"> { ТУТ ЛОГОТИП ИЛИ ТЕКСТ } </button>
+// </div>
+// Для использования просто подключается этот файл к странице
+function showPassword() {
+    document.addEventListener('mousedown', evt => {
+        if(evt.target.classList.contains('password_show_btn')) {
+            const fieldPassword = evt.target.parentElement.querySelector('.input_password');
+            fieldPassword.type = "text";
 
-if(passwordShowBtn) {
-    const fieldPassword = document.querySelector('#login_password');
-    passwordShowBtn.addEventListener('mousedown', () => {
-        fieldPassword.type = "text";
+            evt.target.addEventListener('mouseleave', () => {
+                    fieldPassword.type = "password";
+            });
+            evt.target.addEventListener('mouseup', () => {
+                fieldPassword.type = "password";
+            });
+        }
     });
-    passwordShowBtn.addEventListener('touchstart', () => {
-        fieldPassword.type = "text";
+
+    document.addEventListener('touchstart', evt => {
+        if(evt.target.classList.contains('password_show_btn')) {
+            const fieldPassword = evt.target.parentElement.querySelector('.input_password');
+            fieldPassword.type = "text";
+
+            evt.target.addEventListener('touchend', () => {
+                fieldPassword.type = "password";
+            });
+        }
     });
-    passwordShowBtn.addEventListener('touchend', () => {
-        fieldPassword.type = "password";
-    });
-    passwordShowBtn.addEventListener('mouseup', () => {
-        fieldPassword.type = "password";
-    });
-    passwordShowBtn.addEventListener('mouseleave', () => {
-        fieldPassword.type = "password";
-    });
-}
+};
+document.addEventListener('DOMContentLoaded', showPassword);
 
 const productContainer = document.querySelector('.main_page_product_list_main');
 if(productContainer) {
@@ -40,4 +54,193 @@ if(productContainer) {
             }
         };
     }
-}
+};
+
+// Редактирование информации в аккаунте
+// БЕЗ ОТПРАВКИ ДАННЫХ НА СЕРВЕР
+const accuntInfoContainer = document.querySelector('.main_admin_settings_container');
+if(accuntInfoContainer) {
+    accuntInfoContainer.addEventListener('click', evt => {
+        const btn = evt.target.classList.contains('main_admin_settings_item_info_button') ? evt.target : false;
+        
+        if(btn) {
+            const containerInfo = btn.parentElement;
+            const mainContainer = containerInfo.parentElement;
+
+            if(containerInfo.classList.contains('name')) {
+                editName(containerInfo, mainContainer);
+            } else if (containerInfo.classList.contains('email')) {
+                editEmail(containerInfo, mainContainer);
+            } else if (containerInfo.classList.contains('password')) {
+                editPassword(containerInfo, mainContainer);
+            }
+        }
+    });
+
+    // Изменение имени
+    function editName(container, mainContainer) {
+        const childrenItems = container.cloneNode(true);
+
+        const firstInput = document.createElement('input');
+        const secondInput = document.createElement('input')
+        const saveBtn = document.createElement('button');
+        const cancelBtn = document.createElement('button');
+
+        saveBtn.setAttribute('type', 'button');
+        saveBtn.innerText = "save";
+        saveBtn.classList.add('main_admin_settings_item_info_content_sn_save');
+        
+        cancelBtn.setAttribute('type', 'button');
+        cancelBtn.innerText = "cancel";
+        cancelBtn.classList.add('main_admin_settings_item_info_content_sn_cancel');
+
+        firstInput.classList.add('main_admin_settings_item_info_content_fn');
+        firstInput.setAttribute('placeholder', 'First Name');
+        firstInput.setAttribute('type', 'text');
+        
+        secondInput.classList.add('main_admin_settings_item_info_content_sn');
+        secondInput.setAttribute('placeholder', 'Second Name');
+        secondInput.setAttribute('type', 'text');
+
+        container.innerHTML = "";
+        container.append(firstInput, secondInput, saveBtn, cancelBtn);
+
+        // Сохраняем. Закрываем
+        saveBtn.addEventListener('click', () => {
+            childrenItems.querySelector('#fn').innerText = firstInput.value;
+            childrenItems.querySelector('#sn').innerText = secondInput.value;
+            container.remove();
+            mainContainer.append(childrenItems);
+        });
+
+        // Отменяем. Закрываем
+        cancelBtn.addEventListener('click', () => {
+            container.remove();
+            mainContainer.append(childrenItems);
+        });
+    };
+
+    // Изменение email. 
+    // Проверки валидности email нет
+    // Проверки пароля нет
+    function editEmail(container, mainContainer) {
+        const childrenItems = container.cloneNode(true);
+
+        const firstInput = document.createElement('input');
+        const secondInput = document.createElement('input')
+        const saveBtn = document.createElement('button');
+        const cancelBtn = document.createElement('button');
+        const containerPassword = document.createElement('div');
+        const showPasswordBtn = document.createElement('button');
+
+        showPasswordBtn.setAttribute('type', 'button');
+        showPasswordBtn.classList.add('password_show_btn');
+
+        containerPassword.classList.add('main_login_container_input_password_container');
+
+        firstInput.setAttribute('type', 'email');
+        firstInput.setAttribute('placeholder', 'New Email');
+
+        secondInput.setAttribute('placeholder', 'Account Password');
+        secondInput.setAttribute('type', 'password');
+        secondInput.classList.add('input_password');
+
+        saveBtn.setAttribute('type', 'button');
+        saveBtn.innerText = "+ add";
+        saveBtn.classList.add('main_admin_settings_item_info_content_sn_save');
+        
+        cancelBtn.setAttribute('type', 'button');
+        cancelBtn.innerText = "cancel";
+        cancelBtn.classList.add('main_admin_settings_item_info_content_sn_cancel');
+
+        container.innerHTML = "";
+
+        containerPassword.append(secondInput, showPasswordBtn);
+        container.append(firstInput, containerPassword, saveBtn, cancelBtn);
+
+        // Сохраняем. Закрываем
+        saveBtn.addEventListener('click', () => {
+            childrenItems.querySelector('#email').innerText = firstInput.value;
+            document.querySelector('.header-line_user_login').innerText = firstInput.value;
+            container.remove();
+            mainContainer.append(childrenItems);
+        });
+
+        // Отменяем. Закрываем
+        cancelBtn.addEventListener('click', () => {
+            container.remove();
+            mainContainer.append(childrenItems);
+        });
+    }
+
+    // Изменение пароля
+    // Только фронт-енд часть
+    function editPassword(container, mainContainer) {
+        const childrenItems = container.cloneNode(true);
+
+        const firstInput = document.createElement('input');
+        const secondInput = document.createElement('input')
+        const saveBtn = document.createElement('button');
+        const cancelBtn = document.createElement('button');
+        const containerPasswordF = document.createElement('div');
+        const containerPasswordS = document.createElement('div');
+        const showPasswordBtn = document.createElement('button');
+
+        firstInput.setAttribute('type', 'password');
+        firstInput.setAttribute('placeholder', 'Current Password');
+        firstInput.classList.add('input_password');;
+
+        secondInput.setAttribute('type', 'password');
+        secondInput.setAttribute('placeholder', 'New Password');
+        secondInput.classList.add('input_password');
+
+        showPasswordBtn.setAttribute('type', 'button');
+        showPasswordBtn.classList.add('password_show_btn');
+
+        containerPasswordF.classList.add('main_login_container_input_password_container');
+        containerPasswordS.classList.add('main_login_container_input_password_container');
+
+        saveBtn.setAttribute('type', 'button');
+        saveBtn.setAttribute('disabled', '');
+        saveBtn.innerText = "Confirm";
+        saveBtn.classList.add('main_admin_settings_item_info_content_sn_save');
+        
+        cancelBtn.setAttribute('type', 'button');
+        cancelBtn.innerText = "cancel";
+        cancelBtn.classList.add('main_admin_settings_item_info_content_sn_cancel');
+
+        container.innerHTML = "";
+
+        containerPasswordF.append(firstInput, showPasswordBtn);
+        containerPasswordS.append(secondInput, showPasswordBtn.cloneNode(true));
+
+        container.append(containerPasswordF, containerPasswordS, saveBtn, cancelBtn);
+
+        // Проверяем совпадение паролей. Функция ниже
+        container.addEventListener('input', evt => {
+            if(evt.target.matches('input')) {
+                comparePasswords(firstInput.value, secondInput.value);
+            };
+        });
+
+        // Сохраняем. Закрываем
+        saveBtn.addEventListener('click', () => {
+            container.remove();
+            mainContainer.append(childrenItems);
+        });
+
+        // Отменяем. Закрываем
+        cancelBtn.addEventListener('click', () => {
+            container.remove();
+            mainContainer.append(childrenItems);
+        });
+
+        // Тут соответственно можно добавить запрос с проверкой на соответствие настоящему паролю
+        function comparePasswords(firstPass, secondPass) {
+            if(firstPass.length > "" && secondPass.length >= 7) {
+                saveBtn.removeAttribute('disabled');
+            }
+        }
+    }
+
+};
